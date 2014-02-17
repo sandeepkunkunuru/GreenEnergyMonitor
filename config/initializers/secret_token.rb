87efad345ed1b48c818890a581190aadaaf5418f
Sandeep Kunkunuru <sandeep.kunkunuru@visualiq.com>
2014-02-17 18:02:20 -0500

    Transforming to rails app commit 2

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Greenenergymonitor::Application.config.secret_key_base = 'e7abacc2ff36d703d331a11064929e6d979af47fe10a81a6f319e195ee12982de80f1b52d81152ac4229160c7a180abd893df867e7f380c03a58a1a87a5feaaa'
